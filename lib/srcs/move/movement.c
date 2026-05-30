827a17ebc4c6c8e970cb6cebafe9c8afc7a0f346
Cedric Bouhadra <cebouhad@c4b5c6.42berlin.de>
2026-05-30 19:23:38 +0200

    push_swap_generic

---
+++ lib/srcs/move/movement.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movement.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdric.b <cdric.b@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 09:01:59 by cebouhad          #+#    #+#             */
+/*   Updated: 2026/05/30 08:01:33 by cdric.b          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+int push(t_global_data *data, int src, int dst, int mode)
+{
+	if (!data)
+		return (ERR);
+	if(src == STACK_A && dst == STACK_B && data->size_a)
+	{
+		data->b = data->a;
+		data->size_b++;
+		data->a++;
+		data->size_a--;
+		if (data->size_a == 0)
+			data->a = NULL;
+		if (mode == DISPLAY)
+			print_move(P, dst);
+		return (OK);
+	}
+	if (src == STACK_B && dst == STACK_A && data->size_b)
+	{
+		data->a = data->b;
+		data->size_a++;
+		data->b--;
+		data->size_b--;
+		if (data->size_b == 0)
+			data->b = NULL;
+		if (mode == DISPLAY)
+			print_move(P, dst);
+		return (OK);
+	}
+	return (NO_MOVE);
+}
+
+int swap(t_global_data *data, int stack, int mode)
+{
+	t_stack_data stk;
+
+	if (!data || get_stack_data(data, stack, &stk) == ERR)
+		return (ERR);
+	if (!stk.arr && stk.len < 2)
+		return (NO_MOVE);
+	if (stack == STACK_A)
+		ft_swap(stk.arr, stk.arr + 1);
+	else if(stack == STACK_B)
+		ft_swap(stk.arr, stk.arr - 1);
+	if (mode == DISPLAY)
+		print_move(S, stack);
+	return (OK);
+}
+
+int rotate(t_global_data *data, int stack, int mode)
+{
+	int tmp;
+
+	if (!data || (stack != STACK_A && stack != STACK_B))
+		return (NO_MOVE);
+	if (stack == STACK_A && data->a && data->size_a > 0)
+	{
+		tmp = *(data->a);
+		ft_memmove(data->a, data->a + 1, (data->size_a - 1) * sizeof(int));
+		*(data->a + (data->size_a - 1)) = tmp;
+	}
+	else if (stack == STACK_B && data->b && data->size_b > 0)
+	{
+		tmp = *(data->b);
+		ft_memmove(data->b - (data->size_b - 2), data->b - (data->size_b - 1), (data->size_b - 1) * sizeof(int));
+		*(data->b - (data->size_b  - 1)) = tmp;
+	}
+	
+	if(mode == DISPLAY)
+		print_move(R, stack);
+	return (OK);    
+}
+
+int rev_rotate(t_global_data *data, int stack, int mode)
+{
+	int tmp;
+
+	if (!data || (stack != STACK_A && stack != STACK_B))
+		return (NO_MOVE);
+	if (stack == STACK_A && data->a && data->size_a > 0)
+	{
+		tmp = *(data->a + data->size_a - 1);
+		ft_memmove(data->a + 1, data->a, (data->size_a  - 1) * sizeof(int));
+		*(data->a) = tmp;
+	}
+	else if (stack == STACK_B && data->b && data->size_a > 0)
+	{
+		tmp = *(data->b - (data->size_b - 1));
+		ft_memmove(data->b - (data->size_b - 1), data->b - (data->size_b - 2), (data->size_b - 1) * sizeof(int));
+		*(data->b) = tmp;
+	}
+	
+	if(mode == DISPLAY)
+		print_move(RV, stack);
+	return (OK);    
+}
+
+int double_rotation(t_global_data *data, int move, int mode)
+{
+	if (move != RR && move != RRR)
+		return (NO_MOVE);
+	if (move == RR)
+	{
+		rotate(data, STACK_A, NO_DISPLAY);
+		rotate(data, STACK_B, NO_DISPLAY);
+		if (mode == DISPLAY)
+			print_move(RR, A_AND_B);
+	}
+	else if (move == RRR)
+	{
+		rev_rotate(data, STACK_A, NO_DISPLAY);
+		rev_rotate(data, STACK_B, NO_DISPLAY);
+		if (mode == DISPLAY)
+			print_move(RRR, A_AND_B);
+	}
+	return (OK);
+}
+
---
+++ lib/srcs/utils/utils_2.c
@@ -0,0 +1,134 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cebouhad <cebouhad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 09:51:58 by cebouhad          #+#    #+#             */
+/*   Updated: 2026/05/29 00:19:23 by cebouhad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+int generate_range(int ranges[LIMIT][2], int total_size)
+{
+    int base;
+    int range_start;
+    int range_end;
+    int i;
+
+    if (total_size > LIMIT || total_size < 2)
+        return (ERR);
+    range_start = (total_size - RANGE_SIZE) + 1;
+    range_end = total_size;
+    base = total_size / RANGE_SIZE;
+    if (total_size % RANGE_SIZE)
+        base += 1;
+    i = 0;
+    while (i < base)
+    {
+        ranges[i][0] = range_start;
+        ranges[i][1] = range_end;
+        range_start -= RANGE_SIZE;
+        range_end -= RANGE_SIZE;
+        if(i == base - 1 && ranges[i][0] <= 0)
+            ranges[i][0] = 1;
+        i++;
+    }
+    return (i);
+}
+
+int *bigest_value(t_global_data *data, int stack)
+{
+	int *arr;
+	int *p1;
+	int i = INT_MIN;
+
+	p1 = &i;
+	if (stack == STACK_A)
+	{
+		arr = data->a;
+		while (arr <= data->end)
+		{
+			if(*arr > *p1)
+				p1 = arr;
+			arr++;
+		}
+	}
+	else if(stack == STACK_B)
+	{
+		arr = data->b;
+		while (arr >= data->start)
+		{
+			if(*arr > *p1)
+				p1 = arr;
+			arr--;
+		}
+	}
+	return (p1);
+}
+
+int *smalest_value(t_global_data *data, int stack)
+{
+	int *arr;
+	int *p1;
+	int i = INT_MAX;
+
+	p1 = &i;
+	if (stack == STACK_A)
+	{
+		arr = data->a;
+		while (arr <= data->end)
+		{
+			if(*arr < *p1)
+				p1 = arr;
+			arr++;
+		}
+	}
+	else if(stack == STACK_B)
+	{
+		arr = data->b;
+		while (arr >= data->start)
+		{
+			if(*arr < *p1)
+				p1 = arr;
+			arr--;
+		}
+	}
+	return (p1);
+}
+
+
+int *immediat_superior(t_global_data *data, int stack, int *value)
+{
+	int *arr;
+	int *p1;
+	int i = INT_MAX;
+
+	p1 = &i;
+	if (bigest_value(data, stack) == value)
+		return (value);
+	if (stack == STACK_A)
+	{
+		arr = data->a;
+		while (arr <= data->end)
+		{
+			if(*arr < *p1 && *arr > *value)
+				p1 = arr;
+			arr++;
+		}
+	}
+	else if(stack == STACK_B)
+	{
+		arr = data->b;
+		while (arr >= data->start)
+		{
+			if(*arr < *p1 && *arr > *value)
+				p1 = arr;
+			arr--;
+		}
+	}
+	return (p1);
+}
